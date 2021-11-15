9819defb494fe9d1d2f4f0957606b0039c87b581
kiridihos <47702142+Kiridihos@users.noreply.github.com>
2021-11-14 20:41:41 -0500

    carga dao2

--- src/main/java/com/edu/utadeo/DAO/IEmpleadoDAO.java
+++ src/main/java/com/edu/utadeo/DAO/IEmpleadoDAO.java
@@ -1,5 +1,4 @@
 package com.edu.utadeo.DAO;
-
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.edu.utadeo.modelEntity.Empleado;
 
 public interface IEmpleadoDAO extends JpaRepository<Empleado, UUID> {
-
 }
